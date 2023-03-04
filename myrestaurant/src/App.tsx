07a7b567b9ac232e2ce09f3772e180f5c0989f62
Tracey Ly <traceyly@192.168.1.139>
2023-03-04 12:03:49 +0000

    Config: added typescript to project

--- myrestaurant/src/App.tsx
+++ myrestaurant/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import { Dashboard } from './components/dashboard';
-import Navbar from './components/navbar';
+// import { Dashboard } from './components/dashboard';
+// import Navbar from './components/navbar';
 
 function App() {
 
     return (
         <div className="App">
+            
         </div>
     )
 }
