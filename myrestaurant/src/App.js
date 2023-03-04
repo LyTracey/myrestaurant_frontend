ae5b630d9810ed028b67a5723a7d8c847afca936
Tracey Ly <traceyly@192.168.1.139>
2023-03-04 11:07:11 +0000

    Feature: setup file structure

---
+++ myrestaurant/src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Dashboard } from './components/dashboard';
+import Navbar from './components/navbar';
+
+function App() {
+
+    return (
+        <div className="App">
+        </div>
+    )
+}
+
+export default App;
