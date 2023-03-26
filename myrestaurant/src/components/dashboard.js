8e9f09bfe34841fa7f86c05a6e3989bfe804d5fe
LyTracey <lytracey@hotmail.co.uk>
2023-03-26 02:18:14 +0100

    Feature: added button to modify view by week, month, and quarter

--- myrestaurant/src/components/dashboard.js
+++ myrestaurant/src/components/dashboard.js
@@ -1,4 +1,4 @@
-import { __assign } from "tslib";
+import { __assign, __awaiter, __generator } from "tslib";
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
@@ -8,23 +8,25 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Accordion from 'react-bootstrap/Accordion';
+import Form from 'react-bootstrap/Form';
 import { BarChart, XAxis, YAxis, Tooltip, Bar, ResponsiveContainer } from 'recharts';
-import { profitData, revenueData } from '../data/dashboard_data';
 export function Dashboard(props) {
+    var _this = this;
     // Set states
     var _a = useState({
         low_stock: [],
         out_of_stock: [],
         sales: {},
-        revenue: 0,
-        profit: 0
+        revenue: [],
+        profit: []
     }), statistics = _a[0], setStatistics = _a[1];
     var today = new Date();
     var lastWeek = new Date();
-    lastWeek.setDate(lastWeek.getDate() - 7);
+    lastWeek.setDate(lastWeek.getDate() - 14);
     var _b = useState({
         start_date: lastWeek.toISOString().split('T')[0],
-        end_date: today.toISOString().split('T')[0]
+        end_date: today.toISOString().split('T')[0],
+        frequency: "W"
     }), dateRange = _b[0], setDateRange = _b[1];
     // Define variables
     var endpoint = "http://127.0.0.1:8000/myrestaurant/dashboard/";
@@ -33,30 +35,35 @@ export function Dashboard(props) {
     // Get statistics
     var getStats = function (method, data) {
         if (data === void 0) { data = undefined; }
-        axios({
-            method: method,
-            url: endpoint,
-            data: data
-        })
-            .then(function (response) {
-            setStatistics(__assign({}, response.data));
-        }).catch(function (error) {
-            console.log(error);
+        return __awaiter(_this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, axios({
+                            method: method,
+                            url: endpoint,
+                            data: data
+                        })
+                            .then(function (response) {
+                            setStatistics(__assign({}, response.data));
+                        }).catch(function (error) {
+                            console.log(error);
+                        })];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
         });
     };
     // Get statistics from api
     useEffect(function () {
         getStats("patch", dateRange);
-    }, []);
-    useEffect(function () {
-        console.log(dateRange);
-    });
+    }, [dateRange]);
     // Handle date input
     var handleDate = function (_a) {
         var _b;
         var target = _a.target;
         setDateRange(__assign(__assign({}, dateRange), (_b = {}, _b[target.name] = target.value, _b)));
-        getStats("patch", dateRange);
     };
     // Create cards
     var createAccordion = function (status, title, statistic, comment, keyPrefix) {
@@ -70,11 +77,26 @@ export function Dashboard(props) {
                                         return _jsxs("div", { children: [key, ": ", statistic[key]] }, "".concat(keyPrefix, "-").concat(i));
                                     })) : "No sales made :(") })] })) })) }));
     };
-    // Generate data for graphs
     // Create bar graph crads
-    var createGraph = function (title, data, axisColour, barColour, statistic, dataKey) {
-        return (_jsx(Col, { children: _jsxs(Card, __assign({ className: "stats" }, { children: [_jsx(Card.Title, { children: title }), _jsxs(Card.Body, { children: [_jsx(ResponsiveContainer, __assign({ width: "90%", height: "90%" }, { children: _jsxs(BarChart, __assign({ data: data }, { children: [_jsx(XAxis, { dataKey: "name", stroke: axisColour }), _jsx(YAxis, { stroke: axisColour }), _jsx(Tooltip, {}), _jsx(Bar, { dataKey: dataKey, fill: barColour })] })) })), "Weekly Total: ", Number(statistic) >= 0 ? "\u00A3 ".concat(statistic.toFixed(2)) : "-\u00A3 ".concat(Math.abs(Number(statistic)).toFixed(2))] })] })) }));
+    var createGraph = function (title, data, axisColour, barColour, dataKey) {
+        var _a, _b;
+        var total = data.map(function (item) { return Number(item[dataKey]); }).reduce(function (a, x) { return a + x; }, 0);
+        var dummyData = [
+            (_a = {
+                    date: dateRange.start_date
+                },
+                _a[dataKey] = 0,
+                _a),
+            (_b = {
+                    date: dateRange.end_date
+                },
+                _b[dataKey] = 0,
+                _b)
+        ];
+        return (_jsx(Col, { children: _jsx(Card, __assign({ className: "stats" }, { children: _jsxs(Card.Body, { children: [_jsx(Card.Title, { children: title }), _jsx(ResponsiveContainer, __assign({ width: "90%", height: "75%" }, { children: _jsxs(BarChart, __assign({ data: data.length === 0 ? dummyData : data }, { children: [_jsx(XAxis, { dataKey: "date", stroke: axisColour }), _jsx(YAxis, { stroke: axisColour }), _jsx(Tooltip, {}), _jsx(Bar, { dataKey: dataKey, fill: barColour })] })) })), _jsx(Card.Text, { children: total >= 0 ? "Total: \u00A3 ".concat(total.toFixed(2)) : "Total: -\u00A3 ".concat(Math.abs(total).toFixed(2)) })] }) })) }));
     };
-    return (_jsxs(Container, __assign({ className: "dashboard ".concat(props.theme) }, { children: [_jsx(Row, __assign({ className: 'title justify-content-center' }, { children: _jsx("h2", { children: "Dashboard" }) })), _jsxs(Row, __assign({ lg: 2 }, { children: [_jsx(Col, __assign({ className: "date-range", id: "start-date-range" }, { children: _jsx("input", { type: "date", id: "start-date", name: "start_date", onChange: function (e) { return handleDate(e); } }) })), _jsx(Col, __assign({ className: 'date-range', id: "end-date-range" }, { children: _jsx("input", { type: "date", id: "end-date", name: "end_date", onChange: function (e) { return handleDate(e); } }) }))] })), _jsxs(Row, __assign({ sm: 3, xs: 1, className: 'justify-content-center' }, { children: [createAccordion("error", "Out of Stock", statistics.out_of_stock, "All items in stock!", "out-of-stock"), createAccordion("warning", "Low Stock", statistics.low_stock, "No items low in stock", "low-stock"), createAccordion("success", "Sales", statistics.sales, "No sales made :(", "sales")] })), _jsxs(Row, __assign({ lg: 2, sm: 1, xs: 1, className: 'justify-content-center' }, { children: [createGraph("Revenue", revenueData, props.theme === "light-mode" ? "#7A7A7A" : "#E1E1E1", props.theme === "light-mode" ? "#395C6B" : "#F7EDE2", statistics.revenue, "revenue"), createGraph("Profit", profitData, props.theme === "light-mode" ? "#7A7A7A" : "#E1E1E1", props.theme === "light-mode" ? "#395C6B" : "#F7EDE2", statistics.profit, "profit")] }))] })));
+    var axisColour = props.theme === "light-mode" ? "#7A7A7A" : "#E1E1E1";
+    var barColour = props.theme === "light-mode" ? "#395C6B" : "#F7EDE2";
+    return (_jsxs(Container, __assign({ className: "dashboard ".concat(props.theme) }, { children: [_jsx(Row, __assign({ className: 'title justify-content-center' }, { children: _jsx("h2", { children: "Dashboard" }) })), _jsx(Row, { children: _jsxs(Col, __assign({ className: "date-range" }, { children: [_jsx("input", { type: "date", id: "start-date", name: "start_date", onChange: function (e) { return handleDate(e); } }), _jsx("input", { type: "date", id: "end-date", name: "end_date", onChange: function (e) { return handleDate(e); } }), _jsxs(Form.Select, __assign({ name: 'frequency', onChange: function (e) { return handleDate(e); } }, { children: [_jsx("option", __assign({ value: "W" }, { children: "Weekly" })), _jsx("option", __assign({ value: "M" }, { children: "Monthly" })), _jsx("option", __assign({ value: "Q" }, { children: "Quarterly" }))] }))] })) }), _jsxs(Row, __assign({ lg: 2, sm: 1, xs: 1, className: 'justify-content-center' }, { children: [createGraph("Revenue", statistics.revenue, axisColour, barColour, "revenue"), createGraph("Profit", statistics.profit, axisColour, barColour, "profit")] })), _jsxs(Row, __assign({ sm: 3, xs: 1, className: 'justify-content-center' }, { children: [createAccordion("error", "Out of Stock", statistics.out_of_stock, "All items in stock!", "out-of-stock"), createAccordion("warning", "Low Stock", statistics.low_stock, "No items low in stock", "low-stock"), createAccordion("success", "Sales", statistics.sales, "No sales made :(", "sales")] }))] })));
 }
 ;
--- myrestaurant/src/components/dashboard.tsx
+++ myrestaurant/src/components/dashboard.tsx
@@ -6,9 +6,9 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Accordion from 'react-bootstrap/Accordion';
-import type { Statistics, Sales, DateRange } from '../types/dashboardTypes';
+import Form from 'react-bootstrap/Form';
+import type { Statistics, StatisticsObj, DateRange } from '../types/dashboardTypes';
 import { BarChart, XAxis, YAxis, Tooltip, Bar, ResponsiveContainer} from 'recharts';
-import { profitData, revenueData } from '../data/dashboard_data';
 
 
 export function Dashboard (props: any) {
@@ -19,17 +19,18 @@ export function Dashboard (props: any) {
         low_stock: [],
         out_of_stock: [],
         sales: {},
-        revenue: 0,
-        profit: 0
+        revenue: [],
+        profit: []
     });
 
     const today = new Date();
     const lastWeek = new Date();
-    lastWeek.setDate(lastWeek.getDate() - 7);
+    lastWeek.setDate(lastWeek.getDate() - 14);
 
     const [dateRange, setDateRange] = useState<DateRange>({
         start_date: lastWeek.toISOString().split('T')[0],
-        end_date: today.toISOString().split('T')[0]
+        end_date: today.toISOString().split('T')[0],
+        frequency: "W"
     });
 
 
@@ -40,8 +41,8 @@ export function Dashboard (props: any) {
 
 
     // Get statistics
-    const getStats = (method: string, data: DateRange | undefined = undefined) => {
-        axios({
+    const getStats = async (method: string, data: DateRange | undefined = undefined) => {
+        await axios({
             method: method,
             url: endpoint,
             data: data
@@ -57,20 +58,19 @@ export function Dashboard (props: any) {
     // Get statistics from api
     useEffect(() => {
         getStats("patch", dateRange);
-    }, []);
+    }, [dateRange]);
 
-
+    
     // Handle date input
     const handleDate = ({ target }: any) => {
         setDateRange({
             ...dateRange,
             [target.name]: target.value
         });
-        getStats("patch", dateRange);
     };
 
     // Create cards
-    const createAccordion = (status: string, title: string, statistic: Array<string> | Sales, comment: string, keyPrefix: string) => {
+    const createAccordion = (status: string, title: string, statistic: Array<string> | StatisticsObj, comment: string, keyPrefix: string) => {
         return (
             <Col>
                 <Accordion className="stats col-auto">
@@ -102,62 +102,78 @@ export function Dashboard (props: any) {
         )
     };
 
-    // Generate data for graphs
 
     // Create bar graph crads
-    const createGraph = (title: string, data: any, axisColour: string, barColour: string, statistic: number, dataKey: string) => {
+    const createGraph = (title: string, data: Array<StatisticsObj>, axisColour: string, barColour: string, dataKey: string) => {
+        
+        const total = data.map(item => Number(item[dataKey]) ).reduce((a: number, x: number) => a + x, 0);
+
+        const dummyData = [
+            {
+                date: dateRange.start_date,
+                [dataKey]: 0
+            },
+            {
+                date: dateRange.end_date,
+                [dataKey]: 0
+            }
+        ];
+
+
         return (
             <Col >
                 <Card className="stats">
-                    <Card.Title>{ title }</Card.Title>
                     <Card.Body>
-                    <ResponsiveContainer width="90%" height="90%">
-                        <BarChart data={ data }>
-                            <XAxis dataKey="name" stroke={ axisColour } />
-                            <YAxis stroke={ axisColour } />
-                            <Tooltip />
-                            <Bar dataKey={ dataKey } fill={ barColour } />
-                        </BarChart>
-                    </ResponsiveContainer>
-                    Weekly Total: { Number(statistic) >= 0 ? `£ ${statistic.toFixed(2)}` : `-£ ${Math.abs(Number(statistic)).toFixed(2)}` }
+                        <Card.Title>{ title }</Card.Title>
+                        <ResponsiveContainer width="90%" height="75%">
+                            <BarChart data={ data.length === 0 ? dummyData : data}>
+                                <XAxis dataKey="date" stroke={ axisColour } />
+                                <YAxis stroke={ axisColour } />
+                                <Tooltip />
+                                <Bar dataKey={ dataKey } fill={ barColour } />
+                            </BarChart>
+                        </ResponsiveContainer>
+                        <Card.Text>{ total >= 0 ? `Total: £ ${ total.toFixed(2) }` :  `Total: -£ ${ Math.abs(total).toFixed(2) }` }</Card.Text>
                     </Card.Body>
                 </Card>
             </Col>
         )
     };
 
+    const axisColour = props.theme === "light-mode" ? "#7A7A7A" : "#E1E1E1";
+    const barColour = props.theme === "light-mode" ? "#395C6B" : "#F7EDE2";
 
     return (
         <Container className={ `dashboard ${ props.theme }` }>
-
 
             <Row className='title justify-content-center'>
                     <h2>Dashboard</h2>
             </Row>
 
 
-            <Row lg={2}>
-                <Col className={`date-range`} id="start-date-range">
+            <Row>
+                <Col className={`date-range`}>
                     <input type="date" id="start-date" name="start_date" onChange={e => handleDate(e)}/>
-                </Col>
-                <Col className='date-range' id="end-date-range">
                     <input type="date" id="end-date" name="end_date" onChange={e => handleDate(e)}/>
+                    <Form.Select name='frequency' onChange={e => handleDate(e)}>
+                        <option value="W">Weekly</option>
+                        <option value="M">Monthly</option>
+                        <option value="Q">Quarterly</option>
+                    </Form.Select>
                 </Col>
             </Row>
 
+
+            <Row lg={2} sm={1} xs={1} className='justify-content-center'>
+                { createGraph("Revenue", statistics.revenue, axisColour, barColour, "revenue") }
+                { createGraph("Profit", statistics.profit, axisColour, barColour, "profit") }
+            </Row>
 
             <Row sm={3} xs={1} className='justify-content-center'>
                 { createAccordion("error", "Out of Stock", statistics.out_of_stock, "All items in stock!", "out-of-stock") }
                 { createAccordion("warning", "Low Stock", statistics.low_stock, "No items low in stock", "low-stock") }
                 { createAccordion("success", "Sales", statistics.sales, "No sales made :(", "sales")}
             </Row>
-
-
-            <Row lg={2} sm={1} xs={1} className='justify-content-center'>
-                { createGraph("Revenue", revenueData, props.theme === "light-mode" ? "#7A7A7A" : "#E1E1E1", props.theme === "light-mode" ? "#395C6B" : "#F7EDE2", statistics.revenue, "revenue") }
-                { createGraph("Profit", profitData, props.theme === "light-mode" ? "#7A7A7A" : "#E1E1E1", props.theme === "light-mode" ? "#395C6B" : "#F7EDE2", statistics.profit, "profit") }
-            </Row>
-
 
         </Container>
     )
