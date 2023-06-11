e02268088fe257ae677a4d0dfddfc7e22db8ae59
LyTracey <lytracey@hotmail.co.uk>
2023-06-11 13:23:17 +0100

    Removed dist folder

--- dist/testing/loginTests.js
+++
@@ -1,5 +0,0 @@
-// expect(element).toHaveTextContent(/react/i)
-import '@testing-library/jest-dom';
-// Expect navigation to profile page on successful login
-// Expect feedback if incorrect credientials submitted
-// Expect validation error if username or password not submitted
